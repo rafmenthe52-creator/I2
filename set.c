6f680379b3712aa94e884f33e9729cc397a27032
adriancovagarci-alt <adriancovagarci@gmail.com>
2026-02-18 14:34:44 +0100

    funciones set

---
+++ set.c
@@ -0,0 +1,81 @@
+/**
+ * @brief It works to handle sets
+ *
+ * @file set.c
+ * @author Adrian Covarrubias-AC
+ * @version 1.0
+ * @date 14-02-2026
+ * @copyright GNU Public License
+ */
+
+ #include "set.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct _Set{
+    long *ids;
+    int n_ids;
+};
+
+ Set* set_create(){
+    Set *s=NULL;
+    
+    if(!(s=(Set*)calloc(1, sizeof(Set)))){
+        return NULL;
+    }
+
+    if(!(s->ids=(long*)calloc(s->n_ids, sizeof(long)))){
+        return NULL;
+    }
+    return s;
+ }
+
+  Status set_destroy(Set *s){
+    if(!s){
+        return ERROR;
+    }
+
+    free(s);
+    free(s->ids);
+    s = NULL;
+    s->ids = NULL;
+    return OK;
+  }
+
+   Status set_add(Set *s, long Id){
+    if(!s || Id<=0){
+        return ERROR;
+    }
+
+    s->ids[s->n_ids]=ids;
+    s->n_ids++;
+
+    return OK;
+ }
+
+ Status set_deleate(Set *s, long Id){
+    if(!s || Id<=0 || s->n_ids<=0){
+        return ERROR;
+    }
+
+
+    s->ids[s->n_ids-1]=NO_ID;
+    s->n_ids--;
+
+ }
+
+  Bool set_find(Set *s, long Id){
+    int i;
+    if(!s || Id<=0 || s->n_ids<=0){
+        return FALSE;
+    }
+    for(i=0; i<s->n_ids; i++){
+        if(s->ids[i]!=Id){
+            return FALSE;
+        }else{
+            return TRUE;
+        }
+    }
+
+  }
--- set.h
+++ set.h
@@ -1,5 +1,5 @@
 /**
- * @brief It defines the game interface
+ * @brief It defines the set interface
  *
  * @file set.h
  * @author Adrian Covarrubias-AC
@@ -7,3 +7,67 @@
  * @date 27-01-2025
  * @copyright GNU Public License
  */
+ #ifndef SET_H
+ #define SET_H
+
+ #include "types.h"
+
+ typedef struct _Set Set;
+
+ /**
+ * @brief It initializates a new set
+ * @author Adrian Covarrubias-AC 
+ *
+ * @return It returns the pointer to the structure, or NULL if something goes wrong
+ */
+ Set* set_create();
+
+ /**
+ * @brief It destroys the set
+ * @author Adrian Covarrubias-AC
+ *
+ * @param s a pointer to the struct Set 
+ * @return It returns the Status, either OK or ERROR
+ */
+ Status set_destroy(Set *s);
+
+ /**
+ * @brief It adds a new value to the sets
+ * @author Adrian Covarrubias-AC
+ *
+ * @param s a pointer to the struct Set
+  * @param Id An identificator of ids
+ * @return It returns the Status, either OK or ERROR
+ */
+ Status set_add(Set *s, long Id);
+
+ /**
+ * @brief It deleates a value of the sets
+ * @author Adrian Covarrubias-AC
+ *
+ * @param s a pointer to the struct Set
+ * @param Id An identificator of ids
+ * @return it returns the Status, either OK or ERROR
+ */
+ Status set_deleate(Set *s, long Id);
+
+ /**
+ * @brief Search from an identificator in a set
+ * @author Adrian Covarrubias-AC
+ *
+ * @param s a pointer to the struct Set
+ * @param Id An identificator of ids
+ * @return TRUE if it fins it or FALSE if not
+ */
+ Bool set_find(Set *s, long id);
+
+ /**
+ * @brief Prints the Ids from a set
+ * @author Adrian Covarrubias-AC
+ *
+ * @param s a pointer to the struct Set
+ * @return Void, no return
+ */
+ Void set_print(Set *s);
+
+ #endif
